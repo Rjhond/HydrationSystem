0167c1058ffd2309ac12774e3f069088260144bd
Jhond <142711416+Rjhond@users.noreply.github.com>
2024-05-19 08:09:40 +0800

    Add files via upload

--- hydrationsystem.sql
+++ hydrationsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:4306
--- Generation Time: May 17, 2024 at 03:40 PM
+-- Generation Time: May 19, 2024 at 02:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -63,21 +63,26 @@ CREATE TABLE `users` (
   `securityA` varchar(50) DEFAULT NULL,
   `RegDate` timestamp NULL DEFAULT current_timestamp(),
   `visitTime` timestamp NOT NULL DEFAULT current_timestamp(),
-  `birthdate` date NOT NULL,
+  `birthdate` date NOT NULL DEFAULT '0000-00-00',
   `contact` varchar(15) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `act` varchar(100) NOT NULL,
   `interest` varchar(100) NOT NULL,
-  `bio` varchar(150) NOT NULL
+  `bio` varchar(150) NOT NULL,
+  `actLvl` varchar(100) NOT NULL,
+  `weight` decimal(10,2) NOT NULL,
+  `intake_date` date NOT NULL DEFAULT '0000-00-00',
+  `intake_time` time NOT NULL DEFAULT current_timestamp(),
+  `intake_ml` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`usersid`, `username`, `password`, `fname`, `mname`, `lname`, `securityQ`, `securityA`, `RegDate`, `visitTime`, `birthdate`, `contact`, `email`, `act`, `interest`, `bio`) VALUES
-(1, 'bro', 'Password', 'arjhond', 'g', 'galia', NULL, NULL, '2024-05-15 02:53:36', '2024-05-16 03:52:17', '0000-00-00', '0', '', '', '', ''),
-(2, 'user1', 'Password1', 'bro', '', '', NULL, NULL, '2024-05-16 04:04:05', '2024-05-16 04:04:05', '0000-00-00', '0', '', '', '', '');
+INSERT INTO `users` (`usersid`, `username`, `password`, `fname`, `mname`, `lname`, `securityQ`, `securityA`, `RegDate`, `visitTime`, `birthdate`, `contact`, `email`, `act`, `interest`, `bio`, `actLvl`, `weight`, `intake_date`, `intake_time`, `intake_ml`) VALUES
+(1, 'bro', 'Password', 'arjhond', 'g', 'galia', NULL, NULL, '2024-05-15 02:53:36', '2024-05-16 03:52:17', '0000-00-00', '0', '', '', '', '', '', 0.00, '0000-00-00', '00:00:00', 0),
+(2, 'user1', '1234', 'bro', '', '', 'What is your pet\'s name?', 'enzo', '2024-05-16 04:04:05', '2024-05-16 04:04:05', '0000-00-00', '0', '', '', '', '', '', 0.00, '0000-00-00', '00:00:00', 0);
 
 --
 -- Indexes for dumped tables
